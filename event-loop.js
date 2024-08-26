5152e500ee19c0abcf07ddbce10665a68f03cb36
FrankOHara43 <111985913+FrankOHara43@users.noreply.github.com>
2024-08-26 18:13:23 +0530

    second commit

--- event-loop.js
+++ event-loop.js
@@ -1,34 +1,36 @@
-const fs = require('fs')
-const cryto = require('crypto')
+const fs = require("fs");
+const cryto = require("crypto");
 
-const start = Date.now()
+const start = Date.now();
+// to change thread pool size
+process.env.UV_THREADPOOL_SIZE = 1; // the number of variable here specifies the number of threadpools
 
-setTimeout(() => console.log("Timer 1 finished"), 0)
-setImmediate(() => console.log("Immediate 1 finished"))
+setTimeout(() => console.log("Timer 1 finished"), 0);
+setImmediate(() => console.log("Immediate 1 finished"));
 
 fs.readFile("test-file.txt", () => {
-    console.log("I/O finished")
-    console.log("--------")
-    setTimeout(() => console.log("Timer 2 finished"), 0)
-    setTimeout(() => console.log("Timer 3 finished"), 3000)
-    setImmediate(() => console.log("Immediate 1 finished"))
+  console.log("I/O finished");
+  console.log("--------");
+  setTimeout(() => console.log("Timer 2 finished"), 0);
+  setTimeout(() => console.log("Timer 3 finished"), 3000);
+  setImmediate(() => console.log("Immediate 1 finished"));
 
-    process.nextTick(() => console.log("Process.nextTick"));
+  process.nextTick(() => console.log("Process.nextTick"));
 
-    cryto.pbkdf2('password', 'salt', 10000, 1024, 'sha512', () => {
-        console.log(Date.now() - start, "Password encrypted")
-    })
-    cryto.pbkdf2('password', 'salt', 10000, 1024, 'sha512', () => {
-        console.log(Date.now() - start, "Password encrypted")
-    })
-    cryto.pbkdf2('password', 'salt', 10000, 1024, 'sha512', () => {
-        console.log(Date.now() - start, "Password encrypted")
-    })
-    cryto.pbkdf2('password', 'salt', 10000, 1024, 'sha512', () => {
-        console.log(Date.now() - start, "Password encrypted")
-    })
+  cryto.pbkdf2("password", "salt", 10000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password encrypted");
+  });
+  cryto.pbkdf2("password", "salt", 10000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password encrypted");
+  });
+  cryto.pbkdf2("password", "salt", 10000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password encrypted");
+  });
+  cryto.pbkdf2("password", "salt", 10000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password encrypted");
+  });
 
-    // by default the size of the thread pool is 4
-})
+  // by default the size of the thread pool is 4
+});
 
-console.log("Hello from the top-level code")
+console.log("Hello from the top-level code");
